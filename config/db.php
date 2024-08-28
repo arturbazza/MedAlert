2ac51be07f79094027f9ad76752fd0a139da2eae
arturbazza <agencia@fonzie.com.br>
2024-08-28 08:58:16 -0300

    Ajuste variáveis de ambiente "SERVER_NAME: " . $_SERVER['SERVER_NAME']

--- config/db.php
+++ config/db.php
@@ -1,14 +1,36 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "medalert";
+if ($_SERVER['SERVER_NAME'] === "localhost") {
 
-// Criar conexão
-$conn = new mysqli($servername, $username, $password, $dbname);
+    echo "SERVER_NAME: " . $_SERVER['SERVER_NAME'];
 
-// Verificar conexão
-if ($conn->connect_error) {
-    die("Conexão falhou: " . $conn->connect_error);
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "medalert";
+
+    // Criar conexão
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Verificar conexão
+    if ($conn->connect_error) {
+        die("Conexão falhou: " . $conn->connect_error);
+    }
+} elseif ($_SERVER['SERVER_NAME'] === "medalert.com.br") {
+
+    echo "SERVER_NAME: " . $_SERVER['SERVER_NAME'];
+
+    $servername = "medalert.com.br";
+    $username = "medalert01";
+    $password = "medalert01";
+    $dbname = "medalert01";
+
+    // Criar conexão
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Verificar conexão
+    if ($conn->connect_error) {
+        die("Conexão falhou: " . $conn->connect_error);
+    }
 }
+
 ?>
