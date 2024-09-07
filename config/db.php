0f8202b44d4fcca472189f1a679a6020ef93e078
arturbazza <agencia@fonzie.com.br>
2024-09-07 19:38:23 -0300

    Vender

--- config/db.php
+++ config/db.php
@@ -1,12 +1,31 @@
-# Arquivo .env
-DB_LOCAL_SERVERNAME=localhost
-DB_LOCAL_USERNAME=root
-DB_LOCAL_PASSWORD=
-DB_LOCAL_DBNAME=medalert
-DB_LOCAL_BASE_URL=http://localhost/medalert/
+<?php
+require __DIR__ . '/vendor/autoload.php'; // Autoload do Composer
 
-DB_PROD_SERVERNAME=mysql.medalert.com.br
-DB_PROD_USERNAME=medalert01
-DB_PROD_PASSWORD=pixel1953
-DB_PROD_DBNAME=medalert01
-DB_PROD_BASE_URL=https://medalert.com.br/medalert/
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load(); // Carrega as variáveis de ambiente
+
+if ($_SERVER['SERVER_NAME'] === "localhost") {
+    $servername = $_ENV['DB_LOCAL_SERVERNAME'];
+    $username = $_ENV['DB_LOCAL_USERNAME'];
+    $password = $_ENV['DB_LOCAL_PASSWORD'];
+    $dbname = $_ENV['DB_LOCAL_DBNAME'];
+    $base_url = $_ENV['DB_LOCAL_BASE_URL'];
+} elseif ($_SERVER['SERVER_NAME'] === "medalert.com.br") {
+    $servername = $_ENV['DB_PROD_SERVERNAME'];
+    $username = $_ENV['DB_PROD_USERNAME'];
+    $password = $_ENV['DB_PROD_PASSWORD'];
+    $dbname = $_ENV['DB_PROD_DBNAME'];
+    $base_url = $_ENV['DB_PROD_BASE_URL'];
+}
+
+// Criar conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexão
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+
+// Define a constante BASE_URL
+define('BASE_URL', $base_url);
+?>
