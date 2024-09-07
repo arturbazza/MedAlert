3a621fef5a63ac68e17c2072425b2d418c2c16ca
arturbazza <agencia@fonzie.com.br>
2024-09-07 19:42:50 -0300

    nova compiulçaõ do composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '256c8f77b4e4bae40248d3b105b943fed703e986',
+        'reference' => 'c3bb1e5c7eb36ed2e52894d7892897343206212d',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '256c8f77b4e4bae40248d3b105b943fed703e986',
+            'reference' => 'c3bb1e5c7eb36ed2e52894d7892897343206212d',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
