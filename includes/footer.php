f900f2f43e01e2be63a27285e1d46b8c1e62677f
arturbazza <agencia@fonzie.com.br>
2024-09-07 19:17:50 -0300

    Mudança no footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>Copyright © consit 2024 all right reserved</p>
+    <p>Copyright © consit 2024 all right reserved <a href="https://www.fonzie.com.br" target="_blank"style="text-decoration: none; color: inherit;" title="Visite o site da Fonzie Agência Digital">Fonzie Agência Digital</a></p>
 </footer>
 </body>
 </html>
