a81e82e9b75e10925400e3de80d9215bf1317064
arturbazza <agencia@fonzie.com.br>
2024-10-12 07:22:11 -0300

    Bannar no FOOTER

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,12 @@
+<br>
+<!-- Banner centralizado -->
+<div class="banner">
+    <a href="https://www.weinmann.com.br/" target="_blank">
+        <img src="<?= BASE_URL; ?>imgs/ROSA-20243840x960V3.png" alt="Ir para o site">
+    </a>
+    <!-- span class="banner-text">TEXTO!</!-->
+</div>
+<br>
 <footer>
     <p>Copyright © consit 2024 all right reserved <a href="https://www.fonzie.com.br" target="_blank"style="text-decoration: none; color: inherit;" title="Visite o site da Fonzie Agência Digital">Fonzie Agência Digital</a></p>
 </footer>
